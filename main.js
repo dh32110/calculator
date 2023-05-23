4dbb6d7c2e364056642c3f4898b4001df2e47cb5
dh32110 <dh32110@naver.com>
2023-05-24 01:34:17 +0900

    계산 과정 수정

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let numOne = "";
 let operators = "";
 let numTwo = "";
+let finish = "";
 
-const buttons = document.querySelector(".button");
+const buttons = document.querySelector(".buttons");
 const result = document.querySelector("#result");
 
 function calculate(num1, operator, num2) {
@@ -18,36 +19,41 @@ function calculate(num1, operator, num2) {
    } else if (operator === '/') {
       result.value = num1 / num2;
    }
-   return result;
+   return result.value;
 }
 
-buttons.addEventListener("click", (event) => {
+function onClickBtn(event) {
    const target = event.target;
    const action = target.classList[0];
    const btnContent = target.textContent;
 
    if (target.matches('button')) {
       if (action === 'number') {
-         if (!numOne) {
+         if (!operators) {
             numOne += btnContent;
-            result.value += numOne;
+            result.value += btnContent;
+            console.log(numOne);
          } else {
             numTwo += btnContent;
-            result.value += numTwo;
-         }
-         if (action === 'operator') {
-            operators += btnContent;
-            result.value += operators;
-         }
-         if (action === 'clear') {
-            numOne = "";
-            numTwo = "";
-            operators = "";
-            result = "";
-         }
-         if (action === 'calculate') {
-            result += calculate(numOne, operators, numTwo);
+            result.value += btnContent;
+            console.log(numTwo);
          }
       }
+      if (action === 'operator') {
+         operators += btnContent;
+         result.value += btnContent;
+         console.log(operators);
+      }
+      if (action === 'clear') {
+         numOne = "";
+         numTwo = "";
+         operators = "";
+         result.value = "";
+      }
+      if (action === 'calculate') {
+         result.value += ` = ${calculate(numOne, operators, numTwo)}`;
+      }
    }
-});
+}
+
+buttons.addEventListener("click", onClickBtn);
